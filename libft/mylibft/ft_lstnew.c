b0fe11588d6ebb21314534e7aad360b745b0fb7c
Suzanne Rapin <suzanne.rapin@lilo.org>
2023-06-27 22:33:10 +0200

    libft_normed

--- libft/mylibft/ft_lstnew.c
+++ libft/mylibft/ft_lstnew.c
@@ -6,13 +6,13 @@
 /*   By: srapin <srapin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 04:11:10 by srapin            #+#    #+#             */
-/*   Updated: 2023/06/19 04:03:10 by srapin           ###   ########.fr       */
+/*   Updated: 2023/06/27 22:32:28 by srapin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstnew(void * content)
+t_list	*ft_lstnew(void *content)
 {
 	t_list	*new_lst;
 
