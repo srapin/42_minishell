b047501835d2069ecf718494ff44f91209d1ef95
Helene Lesny <hlesny@student.42.fr>
2023-06-27 22:22:56 +0200

    echo ikuhsohsljnf | 	&& now prints a syntax error

--- src/syntax_pipeline.c
+++ src/syntax_pipeline.c
@@ -6,7 +6,7 @@
 /*   By: hlesny <hlesny@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/23 15:22:29 by Helene            #+#    #+#             */
-/*   Updated: 2023/06/27 21:58:58 by hlesny           ###   ########.fr       */
+/*   Updated: 2023/06/27 22:22:03 by hlesny           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,18 +32,16 @@ void	check_pipe(t_data *data, t_token_list **current)
 {
 	t_token_list	*curr;
 
-	/* while (*current && (*current)->type == whitespace)
-			*current = (*current)->next; */
 	curr = (*current)->next;
 	while (curr && curr->type == whitespace)
 		curr = curr->next;
 	if (!(curr))
-		display_se(data, ft_strdup("newline")); //display_se(data, ft_strdup("|"));
+		display_se(data, ft_strdup("newline"));
 	if ((curr)->type == r_parenthesis)
 		display_se(data, ft_strdup(")"));
 	if ((curr)->type == and_tk)
-		display_se(data, ft_substr((curr)->next->content, 0, 2));
-	(*current) = (*current)->next; // ATTENTION VOIR SI CASSE PAS TOUT
+		display_se(data, ft_substr((curr)->content, 0, 2));
+	(*current) = (*current)->next;
 }
 
 void	check_pipelines(t_data *data, int *parentheses_count)
