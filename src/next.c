187a09a46df47a937be0e21b1a872dd3de8582df
Suzanne Rapin <suzanne.rapin@lilo.org>
2023-06-28 03:07:06 +0200

    normagain

--- src/next.c
+++ src/next.c
@@ -6,18 +6,35 @@
 /*   By: srapin <srapin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 00:50:05 by srapin            #+#    #+#             */
-/*   Updated: 2023/06/28 01:32:57 by srapin           ###   ########.fr       */
+/*   Updated: 2023/06/28 02:55:29 by srapin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+int	get_exit_status(int status, int f_status)
+{
+	if (WIFEXITED(status))
+		f_status = WEXITSTATUS(status);
+	else if (WIFSIGNALED(status))
+	{
+		printf("procces stop with sig %d\n", WTERMSIG(status));
+		f_status = WTERMSIG(status) + 128;
+	}
+	else if (WIFSTOPPED(status))
+	{
+		printf("procces stop with sig %d\n", WTERMSIG(status));
+		f_status = WSTOPSIG(status) + 128;
+	}
+	return (f_status);
+}
+
 void	wait_childs(t_cmd *origin)
 {
-	int	status;
-	int f_status;
-	int ret;
-	t_cmd *cmd;
+	int		status;
+	int		f_status;
+	int		ret;
+	t_cmd	*cmd;
 
 	cmd = origin;
 	ret = g_exit_status;
@@ -29,37 +46,26 @@ void	wait_childs(t_cmd *origin)
 		{
 			f_status = ret;
 			cmd = cmd->red.next_cmd;
-			continue;
+			continue ;
 		}
 		waitpid(cmd->pid, &status, 0);
 		cmd = cmd->red.next_cmd;
-		if (WIFEXITED(status))
-			f_status = WEXITSTATUS(status);
-		else if (WIFSIGNALED(status)) 
-		{
-			printf("procces stop with sig %d\n", WTERMSIG(status));
-            f_status = WTERMSIG(status) + 128;// + 128;
-		}
-		else if (WIFSTOPPED(status))
-		{			
-			printf("procces stop with sig %d\n", WTERMSIG(status));
-            f_status = WSTOPSIG(status) + 128;
-		}
+		f_status = get_exit_status(status, f_status);
 	}
 	g_exit_status = f_status;
 }
 
-bool check_ret(t_cmd *cmd, int ret)
+bool	check_ret(t_cmd *cmd, int ret)
 {
 	if (cmd->ctrl == pointvirgule)
-		return true;
+		return (true);
 	else if (cmd->ctrl == and)
 	{
 		if (ret == 0)
-			return true;
-		return false;
+			return (true);
+		return (false);
 	}
 	if (ret != 0)
-		return true;
-	return false;
+		return (true);
+	return (false);
 }
--- src/safer.c
+++ src/safer.c
@@ -30,127 +30,34 @@ void	safe_pipe(int pipe_tab[2])
 	}
 }
 
-void safe_close_cmd_fd(t_cmd *cmd)
+void	safe_close_cmd_fd(t_cmd *cmd)
 {
 	safe_close(&(cmd->red.in_fd));
 	safe_close(&(cmd->red.out_fd));
 }
 
-
-int is_dir(const char *path)
+bool	check_acces(t_cmd *cmd, t_cmd *first)
 {
-    struct stat path_stat;
-	ft_memset(&path_stat, 0, sizeof(struct stat));
-    stat(path, &path_stat);
-    return S_ISDIR(path_stat.st_mode);
-}
-
-bool	value_is_not_null(t_cmd *cmd)
-{
-	if (!cmd->val.value || !cmd->val.value[0])
-	{
-		errno = ENOENT;
-		return false;
-	}
-	return true;
-}
-
-bool look_like_a_cmd(t_cmd *cmd)
-{
-	return (!(ft_strnstr(cmd->val.value, "./", 2)) && !ft_strnstr(cmd->val.value, "/", 1));
-}
-
-
-bool	check_acces(t_cmd *cmd, t_cmd *first )
-{
-	int		i;
-	char	*path;
-	char	**paths;
-
-	i = 0;
 	if (!value_is_not_null(cmd))
 		return (false);
-	paths = get_path(cmd);
-	while (paths && paths[i] && cmd->val.value)
-	{
-		path = ft_strjoin(paths[i], cmd->val.value);
-		if (path && access(path, X_OK) == 0)
-		{
-			cmd->val.path = path;
-			free_tab(paths);
-			return (true);
-		}
-		free(path);
-		i++;
-	}
-	free_tab(paths);
+	if (try_to_find_path(cmd))
+		return (true);
 	if (is_dir(cmd->val.value))
 	{
 		errno = EISDIR;
-		return false;
+		return (false);
 	}
 	if (look_like_a_cmd(cmd))
 		cmd_not_found(cmd, first);
 	if (access(cmd->val.value, X_OK) != 0)
 		return (false);
-	(void) first;
+	(void)first;
 	cmd->val.path = ft_strdup(cmd->val.value);
 	return (true);
 }
 
-void reset_pip_tab(int pip_tab[2])
+void	reset_pip_tab(int pip_tab[2])
 {
 	pip_tab[0] = -1;
 	pip_tab[1] = -1;
 }
-
-// #include "minishell.h"
-
-// void	safe_close(int *fd)
-// {
-// 	if (*fd > 2)
-// 	{
-// 		close(*fd);
-// 		*fd = -1;
-// 	}
-// }
-
-// void	safe_pipe(int pipe_tab[2])
-// {
-// 	if (pipe(pipe_tab) == 2)
-// 	{
-// 		perror("pipe creation failed");
-// 		exit(EXIT_FAILURE);
-// 	}
-// }
-
-// void	safe_close_cmd_fd(t_cmd *cmd)
-// {
-// 	safe_close(&(cmd->red.in_fd));
-// 	safe_close(&(cmd->red.out_fd));
-// }
-
-// bool	check_acces(t_cmd *cmd, t_cmd *first)
-// {
-// 	if (!value_is_not_null(cmd))
-// 		return (false);
-// 	try_to_find_path(cmd);
-// 	if (is_dir(cmd->val.value))
-// 	{
-// 		errno = EISDIR;
-// 		return (false);
-// 	}
-// 	if (look_like_a_cmd(cmd))
-// 		cmd_not_found(cmd, first);
-// 	if (access(cmd->val.value, X_OK) != 0)
-// 		return (false);
-// 	(void)first;
-// 	cmd->val.path = ft_strdup(cmd->val.value);
-// 	return (true);
-// }
-
-// void	reset_pip_tab(int pip_tab[2])
-// {
-// 	pip_tab[0] = -1;
-// 	pip_tab[1] = -1;
-// }
